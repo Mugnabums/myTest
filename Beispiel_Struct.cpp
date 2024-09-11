13d19ecfb23a57a4563a6ee2381f4baa4ae1b560
Mugnabums <maurizio.mugnaini@audi.de>
2024-09-11 09:03:16 +0200

    Add files via upload

---
+++ Beispiel_Struct.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+struct feld
+{
+    int x {};
+    int y {};
+};
+
+enum class himmelsrichtung {nord, ost, sued, west};
+
+
+feld move (feld aktuellePosition, himmelsrichtung aktuell, bool wall) {
+    feld movement;
+    movement.x = aktuellePosition.x;
+    movement.y = aktuellePosition.y;
+    return movement;
+}
+
+
+
+int main()
+{
+feld XY = move ({1, 2}, himmelsrichtung::nord, true);
+    std::cout << XY.x << '\n';
+    std::cout << XY.y << '\n';
+
+    return 0;
+}
